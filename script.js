c3b3afba6c238f48cc380ee933c24bcde422f8e1
Mossarella <mosno6@gmail.com>
2023-09-23 17:11:24 +0700

    Wrap up

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const videos = document.querySelectorAll('.video');
   
@@ -27,4 +29,59 @@ document.addEventListener('DOMContentLoaded', () => {
       
       });
     });
+  //play video on hover
+
+    const mainVideo = document.querySelector('.mainVideo');
+setTimeout(() => {
+    mainVideo.classList.remove("opacity-0");
+    mainVideo.play()
+}, 4500);
+//autoplay video
+
+
+
+  window.addEventListener("scroll", changeCss , false);
   });
+
+  function changeCss () {
+
+    const mimaneraText = document.querySelector(".mimaneraText");
+
+    const scrollPosition = window.scrollY;
+
+    const isInSection1 = scrollPosition >= section1.offsetTop && scrollPosition < section1.offsetTop + section1.offsetHeight
+    const isInSection2 = scrollPosition >= section2.offsetTop && scrollPosition < section2.offsetTop + section2.offsetHeight
+    const isInSection3 = scrollPosition >= section3.offsetTop && scrollPosition < section3.offsetTop + section3.offsetHeight
+
+   if(isInSection1){
+   
+      if (scrollPosition <= section1.offsetTop) {
+        mimaneraText.style.width ="90%"
+      
+      
+       
+      } else if(scrollPosition >= section1.offsetTop && scrollPosition <= section1.offsetHeight) {
+  
+
+        const distanceToLeftOfSection1 = scrollPosition - section1.offsetTop;
+        const maxWidthDecrease = 100 - ((distanceToLeftOfSection1 / section1.offsetHeight) * 85); // Gradually decrease from 100% to 20%
+        mimaneraText.style.width = `${maxWidthDecrease}%`;
+       
+      }
+      else{
+        mimaneraText.style.width = '15%';
+      }
+
+     
+    }else if (isInSection2) {
+      mimaneraText.style.width ="15%"
+    } else if (isInSection3) {
+      mimaneraText.style.width ="10%"
+    }  
+
+
+
+   
+
+   
+  }
